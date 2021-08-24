a2f81ae831b330488e8d9876f35dcb9142a91c84
julchat <48997230+julchat@users.noreply.github.com>
2021-08-24 16:35:35 -0300

    Update Usuario.java

--- parcial_dds/src/main/java/dominio/Usuario.java
+++ parcial_dds/src/main/java/dominio/Usuario.java
@@ -1,4 +1,30 @@
 package dominio;
-
+import org.apache.commons.lang3.StringUtils;
 public class Usuario {
+    private String usuario;
+    private String password;
+    private Hasheador hasheador = new HasheadorBCrypt();
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean validarLogin(String usuario, String password) {
+        if (StringUtils.isNotEmpty(usuario) && StringUtils.isNotEmpty(password)) {
+            return hasheador.coinciden(hasheador.hashear(password),this.password);
+        } else {
+            return false;
+        }
+    }
 }
